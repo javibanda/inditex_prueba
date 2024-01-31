40e5f68a4b4fb287b88c0cb95a5be2684ccfd4e3
javibanda <narutojavibanda@gmail.com>
2024-01-31 16:28:28 +0100

    Create EndPoint getSimilarProducts

--- src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
+++ src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
@@ -1,6 +1,15 @@
 package com.inditex.zarachallenge.controller;
 
-public class SimilarController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-	// TODO: Insert code in this controller
+@RestController("/")
+public class SimilarController {
+    @GetMapping("/product/{productId}/similar")
+    public ResponseEntity<String> getSimilarProducts(@PathVariable Integer productId){
+        return ResponseEntity.ok("ok");
+    }
+
 }
